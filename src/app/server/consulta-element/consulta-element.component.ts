b8f44943e982d2d1469d4d22b6f95520bca03f02
Arthur <arthurgomesfaria@gmail.com>
2019-11-11 18:51:08 -0300

    Adição de explicitação de Encapsulation

--- src/app/server/consulta-element/consulta-element.component.ts
+++ src/app/server/consulta-element/consulta-element.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ViewEncapsulation } from '@angular/core';
 import { Consulta } from '../consulta';
 @Component({
   selector: '[app-consulta-element]',
   templateUrl: './consulta-element.component.html',
-  styleUrls: ['./consulta-element.component.css']
+  styleUrls: ['./consulta-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // Native, None
 })
 export class ConsultaElementComponent implements OnInit {
 
