6076471fa7e1c19eca124cadc6311e5f194fdf95
Arthur <arthurgomesfaria@gmail.com>
2019-11-10 22:11:21 -0300

    Adicionada lista de consulta

---
+++ src/app/server/consulta.ts
@@ -0,0 +1,8 @@
+export class Consulta {
+    online: boolean;
+    message: string;
+    constructor (online: boolean, message: string) {
+      this.online = online
+      this.message = message
+    }
+  }
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import axios from 'axios';
+import { Consulta } from './consulta'
+
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
 
+
 export class ServerComponent implements OnInit {
 
+  historico = []
   server_url = 'http://localhost:8080/status';
   mostrar_resposta = false
   consultaServer = {
@@ -22,13 +26,13 @@ export class ServerComponent implements OnInit {
   verificaServidor () {
     this.consultaServer.recolhendo = true
     axios.get(this.server_url).then((response) => {
-      console.log(response)
       this.consultaServer.serverStatus = true
       this.consultaServer.resposta = response.data
+      this.historico.push(new Consulta(true, response.data))
     }).catch((error) => {
       this.consultaServer.serverStatus = false
       this.consultaServer.error = error
-      console.log(error)
+      this.historico.push(new Consulta(false, error))
     }).finally( () => {
       this.consultaServer.recolhendo = false
       this.consultaServer.recolhido = true
