f8de49125c22466ab7b41adafdb8556fcc70fd97
Arthur <arthurgomesfaria@gmail.com>
2019-11-24 19:17:30 -0300

    Correção de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://localhost:8080',
+  oracle: 'SERVER=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=MyHost)(PORT=MyPort))(CONNECT_DATA=(SERVICE_NAME=MyOracleSID)));uid=myUsername;pwd=myPassword;'
 };
